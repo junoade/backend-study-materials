695742f62d2ccbc1ba8b5ad60eeed417c92ee0b9
ajchoi0928 <ajchoi0928@gmail.com>
2023-12-30 01:18:54 +0900

    add: springboot2 with java11

---
+++ springboot-2-7-18 with java11/src/main/java/com/example/springboot2718/Springboot2718Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot2718;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2718Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2718Application.class, args);
+    }
+
+}
---
+++ springboot-2-7-18 with java11/src/test/java/com/example/springboot2718/Springboot2718ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot2718;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2718ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
